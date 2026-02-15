7bd96ca0f0accba84e8ac5aae365264fa478d418
DESKTOP-FK5JE54\Albert <albertbabayan94@gmail.com>
2026-02-16 03:33:48 +0400

    has been created project, created routing, material package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {HeaderComponent} from './header/header.component';
-import {MainComponent} from './main/main.component';
+import {HeaderComponent} from './components/header/header.component';
+import {MainComponent} from './components/main/main.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {UploadComponent} from './components/upload/upload.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'upload', pathMatch: 'full'},
+  // {path: 'manage', component: ManageComponent},
+  {path: 'upload', component: UploadComponent},
+];
