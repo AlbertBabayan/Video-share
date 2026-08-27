ca0b9be5ed53cbcfcfd944720215cc91dd7223ea
DESKTOP-FK5JE54\Albert <albertbabayan94@gmail.com>
2026-08-27 20:19:50 +0400

    Has been set two components to one, file upload issue fix, added recent uploads, style changes.

--- src/app/components/custom-dialog/custom-dialog.component.ts
+++ src/app/components/custom-dialog/custom-dialog.component.ts
@@ -16,23 +16,22 @@ import {IElement} from '../../infrastructure/interfaces/element.interface';
 export class CustomDialogComponent {
 
   private dialogRef = inject<DialogRef<IElement>>(DialogRef);
-  public data = inject<IElement>(DIALOG_DATA);
+  public element = inject<IElement>(DIALOG_DATA);
   private formBuilder = inject(FormBuilder);
   public elementForm = this.formBuilder.nonNullable.group({
-    id: [this.data.id, Validators.required],
+    id: [this.element.id, Validators.required],
     name: ['', Validators.required],
     username: ['', Validators.required],
   })
 
   public save() {
-    const name = this.elementForm.get('name')?.value ? this.elementForm.get('name')?.value!: this.data.name;
-    const username = this.elementForm.get('username')?.value ? this.elementForm.get('username')?.value!: this.data.username;
+    const name = this.elementForm.get('name')?.value ? this.elementForm.get('name')?.value!: this.element.name;
+    const username = this.elementForm.get('username')?.value ? this.elementForm.get('username')?.value!: this.element.username;
     this.dialogRef.close({
       id: this.elementForm.get('id')?.value!,
       name: name,
       username: username,
     });
-    console.log(this.elementForm.get('username')?.value)
   }
 
   public cancel() {
--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,26 +1,54 @@
 import {Component, inject, signal} from '@angular/core';
 import {RecordDataService} from '../../services/record-data.service';
-import {RecordComponent} from '../record/record.component';
 import {IElement} from '../../infrastructure/interfaces/element.interface';
 import {elements} from '../../mock/generated-elements';
+import {Dialog} from '@angular/cdk/dialog';
+import {CustomDialogComponent} from '../custom-dialog/custom-dialog.component';
+import {
+  CdkDragDrop,
+ DragDropModule,
+  moveItemInArray,
+  transferArrayItem
+} from '@angular/cdk/drag-drop';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-upload',
   standalone: true,
   imports: [
-    RecordComponent
+    DragDropModule,
+    MatButton
   ],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.scss'
 })
 export class UploadComponent {
 
-  private dataTransferService = inject(RecordDataService);
   private recordService = inject(RecordDataService);
   public elements = signal<IElement[]>([]);
+  public items = signal<IElement[]>([]);
+  private dialog = inject(Dialog);
+  public canShowUploads = signal<boolean>(false);
 
-  public onFileSelect(event: any) {
-    const file = event.target.files && event.target.files[0];
+  public openUpload(input: HTMLInputElement) {
+    this.canShowUploads.set(false);
+    input.click();
+  }
+
+  public uploadVideos(event: Event) {
+    this.onFileSelect(event);
+  }
+
+  public recentUploads() {
+    this.canShowUploads.set(true);
+  }
+
+  public onFileSelect(event: Event) {
+    const eventTarget = event.target as HTMLInputElement;
+    const file = eventTarget.files && eventTarget.files[0];
+    if (!file) {
+      return;
+    }
     if (file) {
       const reader = new FileReader();
       reader.readAsDataURL(file);
@@ -39,9 +67,48 @@ export class UploadComponent {
             ...this.elements(),
             newItem
           ]);
-          this.dataTransferService.changeData(this.elements());
         }
       }
+    }
+  }
+
+  public edit(element: IElement) {
+    const dialogRef = this.dialog.open<
+      IElement
+    >(CustomDialogComponent, {
+      minWidth: '300px',
+      data: element,
+    });
+    dialogRef.closed.subscribe(res => {
+      if (res) {
+        this.elements.update(elements =>
+          elements.map(element =>
+            element.id === res.id
+              ? {...res, record: element.record}
+              : element
+          )
+        );
+        this.items.update(items =>
+          items.map(item =>
+            item.id === res.id
+              ? {...res, record: item.record}
+              : item
+          )
+        );
+      }
+    });
+  }
+
+  public dropItem(event: CdkDragDrop<IElement[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
     }
   }
 }
